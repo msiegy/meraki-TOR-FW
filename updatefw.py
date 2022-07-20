1f2ed5e728b0b31721ef3496e2354ca999dac9ab
Matt Siegel <msiegy88@gmail.com>
2022-07-20 17:44:50 -0400

    Update updatefw.py
    check file age before refreshing

--- updatefw.py
+++ updatefw.py
@@ -1,20 +1,36 @@
 import json
 import requests
 import os
+import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
 MerakiAPIKey = os.environ.get('MerakiAPIKey')
 networkid = os.environ.get('networkid')
 nodesurl = os.environ.get('nodesurl')
+tornodefile = 'tornodes.json'
 
 ipv4 = []
 
-print("Pulling down TOR Guard node list\n")
-response = requests.get(nodesurl)
-open("tornodes.json", "wb").write(response.content)
+##Check if tornodes.json exists, if file age is older than X days pull down fresh copy. else open tornodes.json and continue.
+fileexists = os.path.exists(tornodefile)
+if fileexists:
+    today = datetime.datetime.today()
+    modified_date = datetime.datetime.fromtimestamp(os.path.getmtime(tornodefile))
+    age = today - modified_date
+    if age.days > 3:
 
-f = open ('tornodes.json', "r")
+        print("Pulling down TOR Guard node list\n")
+        response = requests.get(nodesurl)
+        open(tornodefile, "wb").write(response.content)
+    else:
+        print("Skipping TOR file download - last modified:", modified_date)
+else:
+    print("Pulling down TOR Guard node list\n")
+    response = requests.get(nodesurl)
+    open(tornodefile, "wb").write(response.content)
+
+f = open (tornodefile, "r")
   
 # Read TOR Guard Nodes from file and extract IPV4 only addresses
 print("Reading list and formatting for Meraki FW rules\n")
